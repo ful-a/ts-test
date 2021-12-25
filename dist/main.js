e7c93f66273f5cd349b21e9755169299f1e1ab10
ful-a <croqkes@docomo.ne.jp>
2021-12-26 01:16:14 +0900

    try typeScrpt

---
+++ dist/main.js
@@ -0,0 +1,10 @@
+export class Calories {
+    constructor() {
+        this.onigiri = 'onigiri';
+        this.calories = 200;
+    }
+    todaysMeal(food, energy) {
+        return `My ${food} has ${energy} calories`;
+    }
+}
+//# sourceMappingURL=main.js.map
